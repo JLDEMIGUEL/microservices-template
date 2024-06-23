8d7e9e08113e9025fe2f5b19dfab86c53a7bc5ad
jldemiguel <jldmp2001@gmail.com>
2024-06-23 13:05:18 +0200

    Auth server first version

---
+++ auth-server/src/main/java/com/jldemiguel/authserver/service/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.jldemiguel.authserver.service;
+
+import com.jldemiguel.authserver.model.User;
+import com.jldemiguel.authserver.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        log.info("User found with data: {}", user);
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority(user.getRole())));
+    }
+}
