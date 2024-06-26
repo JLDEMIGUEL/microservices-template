3343b68b5f08059d61c53fceff0632dcf7ddeea5
jldemiguel <jldmp2001@gmail.com>
2024-06-26 19:48:09 +0200

    Removed HelloWorld and added Heathcheck

--- microservice-2/src/main/java/com/jldemiguel/microservice2/controller/HealthcheckController.java
+++ microservice-2/src/main/java/com/jldemiguel/microservice2/controller/HealthcheckController.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 
 @RequestMapping("/microservice-2")
 @RestController
-public class HelloWorldController {
+public class HealthcheckController {
 
-    @GetMapping("/hello-world")
-    public ResponseEntity<String> helloWorld(){
-        return ResponseEntity.ok("hello-world");
+    @GetMapping("/healthcheck")
+    public ResponseEntity<Map<String, Object>> helloWorld(){
+        return ResponseEntity.ok(Map.of("status", "UP"));
     }
 }
