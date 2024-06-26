c615cef0d2cc09bc5c19b8f6efb86a591dbd25f3
jldemiguel <jldmp2001@gmail.com>
2024-06-26 19:53:25 +0200

    Change routing of api-gateway

--- microservice-2/src/main/java/com/jldemiguel/microservice2/controller/HealthcheckController.java
+++ microservice-2/src/main/java/com/jldemiguel/microservice2/controller/HealthcheckController.java
@@ -2,13 +2,11 @@ package com.jldemiguel.microservice2.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 
-@RequestMapping("/microservice-2")
 @RestController
 public class HealthcheckController {
 
