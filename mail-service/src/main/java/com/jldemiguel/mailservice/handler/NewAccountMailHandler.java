39e3bb21f76a3abb704142fe8c82cda3605df0dd
jldemiguel <jldmp2001@gmail.com>
2024-07-31 21:29:10 +0200

    Added new account email

--- mail-service/src/main/java/com/jldemiguel/mailservice/handler/NewAccountMailHandler.java
+++ mail-service/src/main/java/com/jldemiguel/mailservice/handler/NewAccountMailHandler.java
@@ -1,16 +1,32 @@
 package com.jldemiguel.mailservice.handler;
 
 import com.jldemiguel.mailservice.model.UserDto;
+import com.jldemiguel.mailservice.service.MailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Map;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class NewAccountMailHandler {
 
+    private static final String NEW_ACCOUNT_SUBJECT = "Welcome %s";
+    private static final String NEW_ACCOUNT_EMAIL_TEMPLATE = "newAccountEmail";
+
+    private final MailService mailService;
+    private final TemplateEngine templateEngine;
+
+
     public void sendNewAccountEmail(UserDto user) {
-        //TODO
+        log.info("Sending new account email {}", user);
+        Context context = new Context();
+        context.setVariables(Map.of("username", user.getUsername(), "email", user.getEmail()));
+        String emailText = templateEngine.process(NEW_ACCOUNT_EMAIL_TEMPLATE, context);
+        mailService.sendEmail(user.getEmail(), NEW_ACCOUNT_SUBJECT.formatted(user.getUsername()), emailText);
     }
 }
