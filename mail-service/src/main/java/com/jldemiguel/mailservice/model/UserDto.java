6d1d53bbdff6d4072d2536c0d8e96d308b9d5064
jldemiguel <jldmp2001@gmail.com>
2024-08-09 15:01:20 +0200

    Fixed missing field in mail-service UserDto

--- mail-service/src/main/java/com/jldemiguel/mailservice/model/UserDto.java
+++ mail-service/src/main/java/com/jldemiguel/mailservice/model/UserDto.java
@@ -1,5 +1,6 @@
 package com.jldemiguel.mailservice.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDto {
     private String username;
     private String email;
