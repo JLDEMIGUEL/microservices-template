ffdb82ebd6a7c02a8ab1192f35552df5674cdee5
jldemiguel <jldmp2001@gmail.com>
2024-08-12 22:12:15 +0200

    Fixed security config for web flux microservice

--- microservice-2/src/main/java/com/jldemiguel/microservice2/config/SecurityConfig.java
+++ microservice-2/src/main/java/com/jldemiguel/microservice2/config/SecurityConfig.java
@@ -2,12 +2,10 @@ package com.jldemiguel.microservice2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,6 +17,9 @@ public class SecurityConfig {
     SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
         http.authorizeExchange(
                         auth -> auth
+                                .pathMatchers("/h2-console/**").permitAll()
+                                .pathMatchers("/actuator/health/livenessState").permitAll()
+                                .pathMatchers("/actuator/health/readinessState").permitAll()
                                 .anyExchange().authenticated()
                 )
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
@@ -30,14 +31,5 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers(
-                new AntPathRequestMatcher("/h2-console/**"),
-                new AntPathRequestMatcher("/actuator/health/livenessState"),
-                new AntPathRequestMatcher("/actuator/health/readinessState")
-        );
     }
 }
