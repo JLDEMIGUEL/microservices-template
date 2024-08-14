e72467198a55128b44dda72d5233d40f72a13db4
jldemiguel <jldmp2001@gmail.com>
2024-08-14 19:19:34 +0200

    Use client-credentials grant type for request between microservices

--- microservice-2/src/main/java/com/jldemiguel/microservice2/client/ProductClient.java
+++ microservice-2/src/main/java/com/jldemiguel/microservice2/client/ProductClient.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -22,7 +21,8 @@ public class ProductClient {
 
     private final WebClient webClient;
 
-    public ProductClient(@Value("${microservice-1.url}") String baseUrl, @Qualifier("lbWebClient") WebClient.Builder webClientBuilder) {
+    public ProductClient(@Value("${microservice-1.url}") String baseUrl,
+                         @Qualifier("lbWebClient") WebClient.Builder webClientBuilder) {
         this.webClient = webClientBuilder.baseUrl(baseUrl + "/product").build();
     }
 
@@ -32,12 +32,6 @@ public class ProductClient {
                 .map(SecurityContext::getAuthentication)
                 .flatMap(authentication -> webClient.get()
                         .uri("/{id}", id)
-                        .headers(headers -> {
-                            log.info("Adding authorization header to request to microservice-1");
-                            if (authentication != null && authentication.getCredentials() instanceof Jwt jwt) {
-                                headers.setBearerAuth(jwt.getTokenValue());
-                            }
-                        })
                         .retrieve()
                         .bodyToMono(Product.class))
                 .onErrorResume(e -> {
--- microservice-2/src/main/java/com/jldemiguel/microservice2/config/WebClientConfig.java
+++ microservice-2/src/main/java/com/jldemiguel/microservice2/config/WebClientConfig.java
@@ -4,6 +4,12 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.InMemoryReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -17,7 +23,25 @@ public class WebClientConfig {
 
     @Bean
     @LoadBalanced
-    public WebClient.Builder lbWebClient() {
-        return WebClient.builder();
+    public WebClient.Builder lbWebClient(ServerOAuth2AuthorizedClientExchangeFilterFunction exchangeFilterFunction) {
+        return WebClient.builder().filter(exchangeFilterFunction);
+    }
+
+    @Bean
+    public ServerOAuth2AuthorizedClientExchangeFilterFunction exchangeFilterFunction(
+            ReactiveClientRegistrationRepository clientRegistrationRepository) {
+        InMemoryReactiveOAuth2AuthorizedClientService clientService =
+                new InMemoryReactiveOAuth2AuthorizedClientService(clientRegistrationRepository);
+        ReactiveOAuth2AuthorizedClientProvider authorizedClientProvider =
+                ReactiveOAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build();
+        AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager authorizedClientManager =
+                new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(
+                        clientRegistrationRepository, clientService);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        ServerOAuth2AuthorizedClientExchangeFilterFunction oauth =
+                new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+        oauth.setDefaultClientRegistrationId("client-microservice-1");
+        return oauth;
     }
 }
